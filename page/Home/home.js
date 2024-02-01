981bfa17b9f4e459444daba1742236b6d6c7d3f3
Cua Dev <dongdun9a4@gmail.com>
2024-02-01 10:57:01 +0700

    feat: add user modal when click user icon

---
+++ page/Home/home.js
@@ -0,0 +1,11 @@
+var modalUser = document.querySelector(".modal_user")
+console.log(modalUser)
+var handleModalUser = () => {
+    if (modalUser.style.display == "none") {
+        modalUser.style.display = "block"
+    }
+    else
+        modalUser.style.display = "none"
+
+}
+
